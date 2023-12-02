7c9f64875324a3b42f381ff18243b0c8c8a38e24
Idkwhoami42 <neellodha42@gmail.com>
2023-12-02 18:15:56 +0100

    add frontend

---
+++ my-app/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link  from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <div className = "flex min-h-screen flex-col justify-center items-center">
+          <Link href="/products"> Products </Link>
+          <Link href="/items"> Items </Link>
+
+      </div>
+    </>
+  );
+}
---
+++ my-app/app/products/[id]/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+
+import Head from "next/head";
+import { usePathname } from "next/navigation";
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
+
+export default function Home() { 
+    const id = usePathname().split("/")[2];
+    const c = ["Bad microphone", "Comment1asdkajlkasjdkladjalsjdlasdjasldasda", "Good", "Okay"]
+    return (
+        <>
+          <Head>
+            <title> LauzHack 2023</title>
+          </Head>
+          <main className="flex justify-center min-h-screen flex-row w-full p-16">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                {/* |issue| Sev level | # reviews | */}
+                    <TableHead>Issue</TableHead>
+                    <TableHead>Severity</TableHead>
+                    <TableHead># Reviews</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {c.map((c, index) => (
+                  index %2 == 0 ? <TableRow className="border-none" key={index}>
+                    <TableCell>{c}</TableCell>
+                    <TableCell>High</TableCell>
+                    <TableCell>42</TableCell>
+                  </TableRow > :
+                    <TableRow key={index} className="bg-muted hover:bg-muted">
+                        <TableCell colSpan={3}>{c}</TableCell>
+                    </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </main>  
+        </>
+        );
+}
