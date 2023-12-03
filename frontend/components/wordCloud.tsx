302707e91a993ca1e5fdc5ff3d83f6e3148e1ea5
Idkwhoami42 <neellodha42@gmail.com>
2023-12-03 06:27:06 +0100

    add word cloud

--- frontend/components/wordCloud.tsx
+++ frontend/components/wordCloud.tsx
@@ -2,7 +2,7 @@ import WordCloud from "react-d3-cloud";
 
 export default function WordCloudUI({data} : {data: any}) {
     return (
-        <WordCloud data = {data} width = {100} height = {100} font="serif" rotate={0}>
+        <WordCloud data = {data} width={100} height={100} rotate={() => (~~(Math.random() * 6) - 3) * 15}>
             
         </WordCloud>
     );
