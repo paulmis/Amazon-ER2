922a0ec058df0fe847c779ec83cf098b07cb395f
Yigit Colakoglu <yigit@yigitcolakoglu.com>
2023-12-03 10:23:39 +0100

    Trying to improve ui

--- frontend/app/[brandName]/products/[name]/issues/page.tsx
+++ frontend/app/[brandName]/products/[name]/issues/page.tsx
@@ -48,62 +48,64 @@ export default function ProductIssuePage() {
   return (
     <>
       <Header/>
-      <main className="flex min-h-screen flex-col w-full p-8 pt-20">
-        <div className="flex items-left justify-center flex-col h-full w-full p-4 ">
-          <div className="flex justify-center">
-            <div className="flex flex-row items-center w-[80%] pb-10">
-              <div className="px-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                </svg>
+      <main className="flex min-h-screen flex-row h-full p-8 pt-20">
+        <div className="flex min-h-screen flex-col w-full p-8">
+          <div className="flex items-left justify-center flex-col w-full p-4 ">
+            <div className="flex justify-center">
+              <div className="flex flex-row items-center w-[80%] pb-10">
+                <div className="px-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                  </svg>
+                </div>
+                <Link href={`/${encodeURIComponent(brandName)}/products/page/1`} className="text-xl py-2 w-[200px]" style={{ whiteSpace: "nowrap" }}>All brands</Link>
+                <div className="text-xl py-2">{name}</div>
               </div>
-              <Link href={`/${encodeURIComponent(brandName)}/products/page/1`} className="text-xl py-2 w-[200px]" style={{ whiteSpace: "nowrap" }}>All brands</Link>
-              <div className="text-xl py-2">{name}</div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-row h-full p-4">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[5%]">Issue</TableHead>
-                <TableHead className="w-[10%]">Details</TableHead>
-                <TableHead className="w-[1%]">Reviews</TableHead>
-                <TableHead className="w-[20%]">Example review</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {clusters.map((cluster, index) => {
-                console.log(cluster)
-                return (
-                  <TableRow key={index}>
-                    <TableCell className="py-[0.39rem]">
-                    <Link href={`/${brandName}/products/${encodeURIComponent(name)}/issue/${encodeURIComponent(cluster.name)}`}>
-                      {capitalizeFirstLetter(cluster.name)}
-                    </Link>
-                    </TableCell>
-                    <TableCell className="py-[0.39rem]">
-                      {[...new Set(cluster.issues.map(issue => issue.issue))].join(", ")}
-                    </TableCell>
-                    <TableCell className="py-[0.39rem]">
-                      {cluster.item_count}
-                    </TableCell>
-                    <TableCell className="py-[0.39rem]">
-                      {
-                        cluster.example.review.length > 150 ?
-                          cluster.example.review.substring(0, 150) + "..." :
-                          cluster.example.review
-                      }
-                    </TableCell>
-                  </TableRow>
-                )
-              })}
-            </TableBody>
-          </Table>
-          <div className="flex flex-col w-[50%]">
-            <WordCloudUI data = { clusters.map(cluster => ({ text: cluster.name, value: cluster.item_count * 3 }))}></WordCloudUI>
-            <Image src={`http://localhost:5000/image?product=${encodeURIComponent(name)}`}></Image>
+          <div className="flex flex-row h-full p-4">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[5%]">Issue</TableHead>
+                  <TableHead className="w-[10%]">Details</TableHead>
+                  <TableHead className="w-[1%]">Reviews</TableHead>
+                  <TableHead className="w-[20%]">Example review</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {clusters.map((cluster, index) => {
+                  console.log(cluster)
+                  return (
+                    <TableRow key={index}>
+                      <TableCell className="py-[0.39rem]">
+                      <Link href={`/${brandName}/products/${encodeURIComponent(name)}/issue/${encodeURIComponent(cluster.name)}`}>
+                        {capitalizeFirstLetter(cluster.name)}
+                      </Link>
+                      </TableCell>
+                      <TableCell className="py-[0.39rem]">
+                        {[...new Set(cluster.issues.map(issue => issue.issue))].join(", ")}
+                      </TableCell>
+                      <TableCell className="py-[0.39rem]">
+                        {cluster.item_count}
+                      </TableCell>
+                      <TableCell className="py-[0.39rem]">
+                        {
+                          cluster.example.review.length > 150 ?
+                            cluster.example.review.substring(0, 150) + "..." :
+                            cluster.example.review
+                        }
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
           </div>
+        </div>
+        <div className="flex flex-col w-[50%] items-center">
+          <Image class="rounded-lg w-[50%] my-4" src={`http://localhost:5000/image?product=${encodeURIComponent(name)}`}></Image>
+          <WordCloudUI data = { clusters.map(cluster => ({ text: cluster.name, value: cluster.item_count * 3 }))}></WordCloudUI>
         </div>
       </main>
     </>
