4d2b164c3961b3c7b0af4b9e6a88a507165787a9
Paul Misterka <paulmist@amazon.com>
2023-12-03 01:03:44 +0100

    badges

--- frontend/components/ui/pagination.tsx
+++ frontend/components/ui/pagination.tsx
@@ -17,7 +17,7 @@ export const Pagination = ({ pageNumber }: { pageNumber: number }) => {
     
 
     return (
-        <div className="w-[20%]  h-10 flex flex-row ">
+        <div className="w-[20%]  h-6 flex flex-row ">
             <Button variant="ghost" onClick={goToPage(pageNumber - 1)} className="">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
